84de1527434abffafe06d78a59c72a21c95d5edb
Fida Hussain <165026313+Fidahussain12@users.noreply.github.com>
2026-05-14 12:04:32 -0700

    Add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import App from "./App.jsx";
 import { HashRouter } from "react-router-dom";  
 import { Provider } from "react-redux";
 import store from "./store/index.js";
+import { SpeedInsights } from "@vercel/speed-insights/react";  
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>  
       <Provider store={store}>
         <App />
+        <SpeedInsights />  
       </Provider>
     </HashRouter>
   </StrictMode>
